926f72f3177d8f15ad16d8641c5c94e6dd00545d
kingsimba <kingsimba@mapbar.com>
2021-02-17 21:14:31 +0800

    add more log functions

--- include/nc-runtime/nc_log.h
+++ include/nc-runtime/nc_log.h
@@ -25,6 +25,10 @@ void Dal_writeLog(const char* str); // (DAL = device adaption layer) device depe
 // clang-format off
 #define __LOG_IMPLE(level, file, line, func, ...) { if (level >= g_ncLogLevel) NcLog_write(level, file, line, func, __VA_ARGS__); }
 
+#define NC_LOG_TRIVIAL(...) __LOG_IMPLE(LogLevel::trivial, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define NC_LOG_INFO(...) __LOG_IMPLE(LogLevel::info, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define NC_LOG_WARNING(...) __LOG_IMPLE(LogLevel::warning, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
 #define NC_LOG_ERROR(...) __LOG_IMPLE(LogLevel::error, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+#define NC_LOG_FATAL(...) __LOG_IMPLE(LogLevel::fatal, __FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
 
 // clang-format on
