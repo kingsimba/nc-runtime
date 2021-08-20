3471b0ec8fb169096fa378a3b3e0cdc579389ea4
kingsimba <kingsimba0511@gmail.com>
2021-08-20 12:06:30 +0800

    fix warnings

--- 3rd-party/src/jansson/utf.cpp
+++ 3rd-party/src/jansson/utf.cpp
@@ -19,23 +19,23 @@ int utf8_encode(int32_t codepoint, char* buffer, size_t* size)
     }
     else if (codepoint < 0x800)
     {
-        buffer[0] = 0xC0 + ((codepoint & 0x7C0) >> 6);
-        buffer[1] = 0x80 + ((codepoint & 0x03F));
+        buffer[0] = (char)(0xC0 + ((codepoint & 0x7C0) >> 6));
+        buffer[1] = (char)(0x80 + ((codepoint & 0x03F)));
         *size = 2;
     }
     else if (codepoint < 0x10000)
     {
-        buffer[0] = 0xE0 + ((codepoint & 0xF000) >> 12);
-        buffer[1] = 0x80 + ((codepoint & 0x0FC0) >> 6);
-        buffer[2] = 0x80 + ((codepoint & 0x003F));
+        buffer[0] = (char)(0xE0 + ((codepoint & 0xF000) >> 12));
+        buffer[1] = (char)(0x80 + ((codepoint & 0x0FC0) >> 6));
+        buffer[2] = (char)(0x80 + ((codepoint & 0x003F)));
         *size = 3;
     }
     else if (codepoint <= 0x10FFFF)
     {
-        buffer[0] = 0xF0 + ((codepoint & 0x1C0000) >> 18);
-        buffer[1] = 0x80 + ((codepoint & 0x03F000) >> 12);
-        buffer[2] = 0x80 + ((codepoint & 0x000FC0) >> 6);
-        buffer[3] = 0x80 + ((codepoint & 0x00003F));
+        buffer[0] = (char)(0xF0 + ((codepoint & 0x1C0000) >> 18));
+        buffer[1] = (char)(0x80 + ((codepoint & 0x03F000) >> 12));
+        buffer[2] = (char)(0x80 + ((codepoint & 0x000FC0) >> 6));
+        buffer[3] = (char)(0x80 + ((codepoint & 0x00003F)));
         *size = 4;
     }
     else
