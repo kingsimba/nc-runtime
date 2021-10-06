4c485282a390a5905c118e14521c80362f3acc1e
kingsimba <kingsimba0511@gmail.com>
2021-10-06 11:47:39 +0800

    faster point in polygon test

--- src/nc_math.cpp
+++ src/nc_math.cpp
@@ -100,113 +100,88 @@ float Math_turnAngle(float from, float to)
     return diff;
 }
 
-/* ======= Crossings algorithm ============================================ */
-// http://www.realtimerendering.com/resources/GraphicsGems//gemsiv/ptpoly_haines/ptinpoly.cpp
+/* ======= Crossings Multiply algorithm =================================== */
+/* http://www.realtimerendering.com/resources/GraphicsGems//gemsiv/ptpoly_haines/ptinpoly.cpp */
 
-/* Shoot a test ray along +X axis.  The strategy, from MacMartin, is to
- * compare vertex Y values to the testing point's Y and quickly discard
- * edges which are entirely to one side of the test ray.
+/*
+ * This version is usually somewhat faster than the original published in
+ * Graphics Gems IV; by turning the division for testing the X axis crossing
+ * into a tricky multiplication test this part of the test became faster,
+ * which had the additional effect of making the test for "both to left or
+ * both to right" a bit slower for triangles than simply computing the
+ * intersection each time.  The main increase is in triangle testing speed,
+ * which was about 15% faster; all other polygon complexities were pretty much
+ * the same as before.  On machines where division is very expensive (not the
+ * case on the HP 9000 series on which I tested) this test should be much
+ * faster overall than the old code.  Your mileage may (in fact, will) vary,
+ * depending on the machine and the test data, but in general I believe this
+ * code is both shorter and faster.  This test was inspired by unpublished
+ * Graphics Gems submitted by Joseph Samosky and Mark Haigh-Hutchinson.
+ * Related work by Samosky is in:
+ *
+ * Samosky, Joseph, "SectionView: A system for interactively specifying and
+ * visualizing sections through three-dimensional medical image data",
+ * M.S. Thesis, Department of Electrical Engineering and Computer Science,
+ * Massachusetts Institute of Technology, 1993.
+ *
+ */
+
+/* Shoot a test ray along +X axis.  The strategy is to compare vertex Y values
+ * to the testing point's Y and quickly discard edges which are entirely to one
+ * side of the test ray.  Note that CONVEX and WINDING code can be added as
+ * for the CrossingsTest() code; it is left out here for clarity.
  *
  * Input 2D polygon _pgon_ with _numverts_ number of vertices and test point
- * _point_, returns 1 if inside, 0 if outside.    WINDING and CONVEX can be
- * defined for this test.
+ * _point_, returns 1 if inside, 0 if outside.
  */
 bool Math_pointInPolygon(const Vector2* pgon, size_t numverts, Vector2 pt)
 {
     if (numverts <= 2)
         return false;
 
-#ifdef WINDING
-    int crossings;
-#endif
-    int j, yflag0, yflag1, inside_flag, xflag0;
-    float ty, tx;
-    const Vector2* vtx0;
-    const Vector2* vtx1;
-#ifdef CONVEX
-    int line_flag;
-#endif
+    bool inside = false;
+    bool yflag0, yflag1;
 
-    tx = pt.x;
-    ty = pt.y;
+    const Vector2* vtx0 = &pgon[numverts - 1];
+    const Vector2* vtx1 = &pgon[0];
 
-    vtx0 = &pgon[numverts - 1];
     /* get test bit for above/below X axis */
-    yflag0 = (vtx0->y >= ty);
-    vtx1 = &pgon[0];
+    yflag0 = (vtx0->y >= pt.y);
 
-#ifdef WINDING
-    crossings = 0;
-#else
-    inside_flag = 0;
-#endif
-#ifdef CONVEX
-    line_flag = 0;
-#endif
-    for (j = (int)numverts + 1; --j;)
+    for (int j = (int)numverts + 1; --j;)
     {
-
-        yflag1 = (vtx1->y >= ty);
-        /* check if edge's endpoints straddle (are on opposite sides) of X axis
+        yflag1 = (vtx1->y >= pt.y);
+        /* Check if endpoints straddle (are on opposite sides) of X axis
          * (i.e. the Y's differ); if so, +X ray could intersect this edge.
+         * The old test also checked whether the endpoints are both to the
+         * right or to the left of the test point.  However, given the faster
+         * intersection point computation used below, this test was found to
+         * be a break-even proposition for most polygons and a loser for
+         * triangles (where 50% or more of the edges which survive this test
+         * will cross quadrants and so have to have the X intersection computed
+         * anyway).  I credit Joseph Samosky with inspiring me to try dropping
+         * the "both left or both right" part of my code.
          */
         if (yflag0 != yflag1)
         {
-            xflag0 = (vtx0->x >= tx);
-            /* check if endpoints are on same side of the Y axis (i.e. X's
-             * are the same); if so, it's easy to test if edge hits or misses.
+            /* Check intersection of pgon segment with +X ray.
+             * Note if >= point's X; if so, the ray hits it.
+             * The division operation is avoided for the ">=" test by checking
+             * the sign of the first vertex wrto the test point; idea inspired
+             * by Joseph Samosky's and Mark Haigh-Hutchinson's different
+             * polygon inclusion tests.
              */
-            if (xflag0 == (vtx1->x >= tx))
+            if (((vtx1->y - pt.y) * (vtx0->x - vtx1->x) >= (vtx1->x - pt.x) * (vtx0->y - vtx1->y)) == yflag1)
             {
-
-                /* if edge's X values both right of the point, must hit */
-#ifdef WINDING
-                if (xflag0)
-                    crossings += (yflag0 ? -1 : 1);
-#else
-                if (xflag0)
-                    inside_flag = !inside_flag;
-#endif
+                inside = !inside;
             }
-            else
-            {
-                /* compute intersection of polygon edge with +X ray, note
-                 * if >= point's X; if so, the ray hits it.
-                 */
-                if ((vtx1->x - (vtx1->y - ty) * (vtx0->x - vtx1->x) / (vtx0->y - vtx1->y)) >= tx)
-                {
-#ifdef WINDING
-                    crossings += (yflag0 ? -1 : 1);
-#else
-                    inside_flag = !inside_flag;
-#endif
-                }
-            }
-#ifdef CONVEX
-            /* if this is second edge hit, then done testing */
-            if (line_flag)
-                goto Exit;
-
-            /* note that one edge has been hit by the ray's line */
-            line_flag = TRUE;
-#endif
-            /* small improvement over original code: update yflag0 only when
-             * we know it differs from yflag1.
-             */
-            yflag0 = yflag1;
         }
 
-        /* move to next pair of vertices, retaining info as possible */
+        /* Move to the next pair of vertices, retaining info as possible. */
+        yflag0 = yflag1;
         vtx0 = vtx1;
         vtx1++;
     }
-#ifdef CONVEX
-Exit:;
-#endif
-#ifdef WINDING
-    /* test if crossings is not zero */
-    inside_flag = (crossings != 0);
-#endif
 
-    return (inside_flag);
+    return inside;
 }
--- test/nc_math_unittest.cpp
+++ test/nc_math_unittest.cpp
@@ -58,3 +58,58 @@ TEST(Math, Math_pointInPolygon)
         EXPECT_FALSE(Math_pointInPolygon(points.data(), points.size(), Vector2(10.01f, 5)));
     }
 }
+
+// #define POINT_COUNT 100000000
+#define POINT_COUNT 100
+static vector<Vector2> g_points;
+static vector<Vector2> g_polygon{{90.3515625f, 67.47492238478702f},
+                                 {81.2109375f, 65.2198939361321f},
+                                 {79.453125f, 55.3791104480105f},
+                                 {78.3984375f, 39.095962936305476f},
+                                 {113.5546875f, 36.03133177633187f},
+                                 {124.45312499999999f, 42.5530802889558f},
+                                 {135.0f, 67.47492238478702f},
+                                 {126.91406249999999f, 69.53451763078358f},
+                                 {110.390625f, 72.91963546581484f},
+                                 {100.8984375f, 73.42842364106816f},
+                                 {95.2734375f, 73.42842364106816f},
+                                 {79.453125f, 74.21198251594369f},
+                                 {69.9609375f, 72.71190310803662f},
+                                 {55.1953125f, 70.61261423801925f},
+                                 {59.0625f, 68.00757101804004f},
+                                 {70.3125f, 69.90011762668541f},
+                                 {77.34374999999999f, 70.37785394109224f},
+                                 {89.6484375f, 70.49557354093136f},
+                                 {95.2734375f, 70.37785394109224f},
+                                 {104.765625f, 68.39918004344189f},
+                                 {110.0390625f, 65.5129625532949f},
+                                 {104.0625f, 61.60639637138628f},
+                                 {99.140625f, 55.57834467218206f},
+                                 {94.21875f, 54.77534585936447f},
+                                 {96.328125f, 61.938950426660604f},
+                                 {96.328125f, 65.2198939361321f},
+                                 {90.3515625f, 67.47492238478702f}};
+
+class PointInPolygonTest : public ::testing::Test
+{
+public:
+    static void SetUpTestCase()
+    {
+        g_points.reserve(POINT_COUNT);
+        for (int i = 0; i < POINT_COUNT; i++)
+        {
+            Vector2 v;
+            v.x = (float)(rand() % 50) + 60;
+            v.y = (float)(rand() % 30) + 30;
+            g_points.push_back(v);
+        }
+    }
+};
+
+TEST_F(PointInPolygonTest, performance)
+{
+    for (Vector2 v : g_points)
+    {
+        Math_pointInPolygon(g_polygon.data(), g_polygon.size(), v);
+    }
+}
