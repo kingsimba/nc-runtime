6d5fc9db49e8fb4817839048ccc9ecf6cc0fba5d
FengZhaolin <kingsimba0511@gmail.com>
2022-01-21 12:58:10 +0800

    add Pose2::oriInDegree()

--- include/nc_runtime/pose2.h
+++ include/nc_runtime/pose2.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "nc_types.h"
 #include "vector2.h"
 
 namespace nc
@@ -10,6 +11,8 @@ class Pose2
 public:
     Vector2 pos;
     float ori;
+
+    float oriInDegree() const { return nc_radianToDegree(this->ori); }
 
     Pose2() = default;
     Pose2(float x, float y, float ori_) : pos(x, y), ori(ori_) {}
